0f34d74dbdec8be630bbaffdf8a797b97ba430d6
Caner Gedik <130908718+canergedik28@users.noreply.github.com>
2025-12-17 05:01:48 +0300

    Improve log file reading and user choice handling
    Refactor log reading and user interaction logic.

--- log-parser/logAnalyst.py
+++ log-parser/logAnalyst.py
@@ -1,28 +1,66 @@
 import re
 from collections import defaultdict
 
+
 class AuthLogAnalyst: 
-   
+    
     def __init__(self) -> None:
        pass
-
-    def read_log_file(self,log_file="test.log"):
-        with open(log_file,'r') as file:
-            file = file.read()
-        return file
     
-
+    def read_log_file(self,file_path="test.log"):
+            with open(file_path,'r') as file:
+                file = file.read()
+            return file
+    
     def get_auth_log_analyst(self):
-        count = 1
         log_file = self.read_log_file()
         data = re.findall(r'Failed password for invalid user (.*?) from (.*?) ',log_file)
         grouped_data = defaultdict(list)
         for username, ip in data:
             grouped_data[ip].append(username)
-        for ip, usernames in grouped_data.items():
-            print(f"{count} - IP: {ip} -> Users: {', '.join(usernames)} ->  attack_count: {len(usernames)}" + "\n\n\n")
-            count+=1
+        return grouped_data.items()
+    
+    def get_sliced_list(self,limit=20):
+        data =  list(self.get_auth_log_analyst())[:limit]
+        return data
+    
+    def get_reversed_list_slice(self,limit=-20):
+        data =  list(self.get_auth_log_analyst())[limit:][::-1]
+        return data
 
 if(__name__ == "__main__"):
     authLogAnalyst = AuthLogAnalyst()
-    authLogAnalyst.get_auth_log_analyst()
+    control = 1
+    while (control == 1  or control == 3) or control == 4:
+      try: 
+          count = 1
+          choice = int(input("Bir secim islemi yapiniz:"))
+          if choice == 1:
+            data = authLogAnalyst.get_auth_log_analyst()
+            for ip, usernames in data:
+              print(f"{count} - IP: {ip} -> Users: {', '.join(usernames)} ->  attack_count: {len(usernames)}" + "\n\n\n")
+              count +=1
+          elif choice == 2:
+            limit = int(input("Bir limit giriniz: "))
+            data = authLogAnalyst.get_sliced_list(limit=limit)
+            for ip, usernames in data:
+              print(f"{count} - IP: {ip} -> Users: {', '.join(usernames)} ->  attack_count: {len(usernames)}" + "\n\n\n")
+              count +=1
+          elif choice == 3:
+            limit = int(input("Bir limit giriniz: "))
+            data = authLogAnalyst.get_reversed_list_slice(limit=-limit)
+            for ip, usernames in data:
+              print(f"{count} - IP: {ip} -> Users: {', '.join(usernames)} ->  attack_count: {len(usernames)}" + "\n\n\n")
+              count +=1
+          elif choice == 4:
+            text = str(input("Bir text giriniz: "))
+            limit = int(input("Bir limit giriniz: "))
+            data = authLogAnalyst.get_sliced_list(limit=limit)
+            for ip, usernames in data:
+               print(f"{count} - IP: {ip} -> Users: {', '.join([search for search in usernames if search.startswith(text) ])} ->  attack_count: {len([search for search in usernames if search.startswith(text) ])}" + "\n\n\n")
+               count+=1
+          else:
+            control = 5
+      except:
+          control = 4
+
